8eea7d6df85b4afcd4a8bff1f8b25dc6d9d8d94c
Dave Evans <dave.evans55@googlemail.com>
2021-04-06 15:58:24 +0100

    Fix js bug

--- find_numbers.js
+++ find_numbers.js
@@ -4,10 +4,11 @@ var anchors = document.getElementsByTagName("a");
 
 for(let i = 0, len = anchors.length; i < len; i++) {
     if(anchors[i].getAttribute("href").startsWith("tel:")) {
-        let tel = anchors[i].getAttribute("href").split(":").pop();
-	console.log("Grandstream CTC: original tel = " + tel);
-	tel = tel.replace(/-/g,"");
-	tel = tel.replace(/^\+/,"00"); // 00 is International Dialling prefix in United Kingdom  
+        xtel = anchors[i].getAttribute("href").split(":").pop();
+	console.log("Grandstream CTC: original tel = " + xtel);
+	xtel = xtel.replace(/-/g,"");
+	// 00 is International Dialling prefix in United Kingdom  
+	let tel = xtel.replace(/^\+/,"00"); 
         console.log("Grandstream CTC: Replaced " + tel);
         anchors[i].setAttribute("href", "#");
 
