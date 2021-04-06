10c6b1bea6f9fcd5672d309b555a9f08201099cf
Dave Evans <dave.evans55@googlemail.com>
2021-04-06 14:41:36 +0100

    find_numbers.js
    Fix bug if multiple tel URLs on page it would always dial the last one.
    
    Strip hyphens from number, replace leading + by International Dialling Prefix
    for the UK.

--- find_numbers.js
+++ find_numbers.js
@@ -1,8 +1,13 @@
+// github/jabelone code modified by github/snowflake 5 April 2021
+
 var anchors = document.getElementsByTagName("a");
 
-for(var i = 0, len = anchors.length; i < len; i++) {
+for(let i = 0, len = anchors.length; i < len; i++) {
     if(anchors[i].getAttribute("href").startsWith("tel:")) {
-        var tel = anchors[i].getAttribute("href").split(":").pop();
+        let tel = anchors[i].getAttribute("href").split(":").pop();
+	console.log("Grandstream CTC: original tel = " + tel);
+	tel = tel.replace(/-/g,"");
+	tel = tel.replace(/^\+/,"00"); // 00 is International Dialling prefix in United Kingdom  
         console.log("Grandstream CTC: Replaced " + tel);
         anchors[i].setAttribute("href", "#");
 
@@ -12,3 +17,5 @@ for(var i = 0, len = anchors.length; i < len; i++) {
         }
     }
 }
+
+
