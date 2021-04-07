3dd9dabb6dc10182fbcbdc74d2f9924a5c924f95
Dave Evans <dave.evans55@googlemail.com>
2021-04-07 13:52:03 +0100

    Update maniest to v2.0.2
    find_numbers.js: improve scanning of anchors.
    ( fix from github/vquintans )

--- find_numbers.js
+++ find_numbers.js
@@ -3,7 +3,7 @@
 var anchors = document.getElementsByTagName("a");
 
 for(let i = 0, len = anchors.length; i < len; i++) {
-    if(anchors[i].getAttribute("href").startsWith("tel:")) {
+    if(anchors[i].getAttribute("href") && anchors[i].getAttribute("href").startsWith("tel:")) {
         xtel = anchors[i].getAttribute("href").split(":").pop();
 	console.log("Grandstream CTC: original tel = " + xtel);
 	xtel = xtel.replace(/-/g,"");
